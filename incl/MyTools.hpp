f962146eb91a8ba94e45c02b5cf085e3f23a4b70
yoda <pascal@jolidon.ch>
2026-02-05 22:23:04 +0100

    pushed

--- incl/MyTools.hpp
+++ incl/MyTools.hpp
@@ -5,6 +5,28 @@
 #include <iostream>
 #include <cstdlib>
 
+#ifndef RESET
+# define RESET   "\033[0m"
+#endif
+#ifndef RED
+# define RED	 	"\033[31m"
+#endif
+#ifndef GREEN
+# define GREEN   "\033[32m"
+#endif
+#ifndef YELLOW
+# define YELLOW  "\033[33m"
+#endif
+#ifndef BLUE
+# define BLUE	"\033[34m"
+#endif
+#ifndef MAGENTA
+# define MAGENTA "\033[35m"
+#endif
+#ifndef CYAN
+# define CYAN	"\033[36m"
+#endif
+
 #ifndef MYTOOLS_HPP
 # define MYTOOLS_HPP
 
